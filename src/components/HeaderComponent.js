8cb142bc63319eea94aaddfae430b9d594cd2840
gpedraza1990 <gabriel1990@tutanota.com>
2019-04-01 09:57:55 -0400

    adding a leader, dish and promotion to home page

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -47,7 +47,7 @@ class Header extends Component{
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/constactus">
+                                    <NavLink className="nav-link" to="/contactus">
                                     <span className="fa fa-address-card fa-lg"></span>Contact Us
                                     </NavLink>
                                 </NavItem>
